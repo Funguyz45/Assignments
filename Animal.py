93df16621773958da88d7b233ca6f32d37acdcf7
Funguyz45 <114613001+Funguyz45@users.noreply.github.com>
2022-09-28 12:10:04 -0700

    Add files via upload

---
+++ Animal.py
@@ -0,0 +1,8 @@
+# Author: Ethan Dean Roumeliotis
+# GitHub username: Funguyz45
+# Date: 9/25/2022
+# Description: Asks the user for their favorite animal and then prints out that
+
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
